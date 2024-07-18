5230431d2bcdbb38e2c64991519ef7957616716a
Aniket Jas <aniketd232@gmail.com>
2024-07-18 20:00:02 +0530

    Account page, logout btn working

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -7,14 +7,14 @@ export default function Header() {
 
   return (
     <header className=" flex justify-between">
-      <a href="" className="flex items-center gap-1">
+      <Link to="/" className="flex items-center gap-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="size-8 -rotate-90 text-primary"
+          className="size-8 -rotate-90"
         >
           <path
             strokeLinecap="round"
@@ -22,8 +22,8 @@ export default function Header() {
             d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
           />
         </svg>
-        <span className="text-xl text-primary font-bold">airbnb</span>
-      </a>
+        <span className="text-xl font-bold">airbnb</span>
+      </Link>
 
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
@@ -50,7 +50,7 @@ export default function Header() {
       </div>
 
       <Link
-        to={"/login"}
+        to={user ? "/account" : "/login"}
         className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4"
       >
         <svg
--- server/index.js
+++ server/index.js
@@ -70,13 +70,18 @@ app.post("/login", async (req, res) => {
 app.get("/profile", (req, res) => {
   const { token } = req.cookies;
   if (token) {
-    jwt.verify(token, jwtSecret, {}, (err, user) => {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
       if (err) throw err;
-      res.json(user);
+      const { name, email, _id } = await UserModel.findById(userData.id);
+      res.json({ name, email, _id });
     });
   } else {
     res.json(null);
   }
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
 });
 
 app.listen(port, () => {
